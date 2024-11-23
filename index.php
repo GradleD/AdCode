fe41be4991284ce272efc3d79bcb42b5e3ddeff1
Darshan Chauhan <46072647+GradleD@users.noreply.github.com>
2024-11-23 19:36:19 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+
+echo 'Hello';
+
+
+?>
+
+<html>
+  <head>
+    <title>Ad testing</title>
+
+    <script data-cfasync="false" type="text/javascript" id="clever-core">
+/* <![CDATA[ */
+    (function (document, window) {
+        var a, c = document.createElement("script"), f = window.frameElement;
+
+        c.id = "CleverCoreLoader86832";
+        c.src = "https://scripts.cleverwebserver.com/97ada3899ffea4d534963c83e2917f4f.js";
+
+        c.async = !0;
+        c.type = "text/javascript";
+        c.setAttribute("data-target", window.name || (f && f.getAttribute("id")));
+        c.setAttribute("data-callback", "put-your-callback-function-here");
+        c.setAttribute("data-callback-url-click", "put-your-click-macro-here");
+        c.setAttribute("data-callback-url-view", "put-your-view-macro-here");
+        
+
+        try {
+            a = parent.document.getElementsByTagName("script")[0] || document.getElementsByTagName("script")[0];
+        } catch (e) {
+            a = !1;
+        }
+
+        a || (a = document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0]);
+        a.parentNode.insertBefore(c, a);
+    })(document, window);
+/* ]]> */
+</script>
+  </head>
+  <body>
+    <h1>Ad Code testing...</h1>
+  </body>
+</html>
